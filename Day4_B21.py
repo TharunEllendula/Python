653ef9c2b058da133e088307fbc67b2dac023d6e
TharunEllendula <48919558+TharunEllendula@users.noreply.github.com>
2020-06-28 18:59:26 +0530

    Add files via upload

---
+++ Day4_B21.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+##Introduction to list datatype
+
+
+# In[9]:
+
+
+#def:list is defined as order collection of items
+
+
+# In[1]:
+
+
+To difine a list we use -----> []
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+students = ['ram','shiva','sai','sita']
+
+
+# In[2]:
+
+
+type(students)
+
+
+# In[ ]:
+
+
+#assigning the individual elements on the opt:
+
+
+# In[ ]:
+
+
+Intro to index : it starts with 0,1,2,3,4,5.........
+
+
+# In[3]:
+
+
+students[1]
+
+
+# In[ ]:
+
+
+How to modify,alter,delete the elements in the list data type
+
+
+# In[4]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+## Modify the the data shiva ------> shivam
+
+
+# In[5]:
+
+
+students[1] = 'shivam'
+
+
+# In[6]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+## Append(add's at last) the the data --------> radha
+
+
+# In[ ]:
+
+
+
+
+
+# In[7]:
+
+
+students.append('radha')
+
+
+# In[8]:
+
+
+print(students)
+
+
+# In[10]:
+
+
+## append at selceted place means insert 
+
+
+# In[12]:
+
+
+print(students)
+
+
+# In[14]:
+
+
+students.insert(1, 'hari')
+
+
+# In[15]:
+
+
+print(students)
+
+
+# In[16]:
+
+
+print(students[1])
+
+
+# In[ ]:
+
+
+
+
+
+# In[17]:
+
+
+#how to del the element from list
+
+
+# In[18]:
+
+
+print(students)
+
+
+# In[20]:
+
+
+del students[4]
+
+
+# In[21]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
